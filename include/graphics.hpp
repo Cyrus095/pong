153c86bb77e1874165e4b0a3ebd63cd8cbdfdc97
Leonardo Pereira Macedo <leonardo.macedo@usp.br>
2015-02-16 22:05:51 -0200

    Tested openGL by drawing a white triangle

--- include/graphics.hpp
+++ include/graphics.hpp
@@ -10,36 +10,67 @@
 class Graphics
 {
     private:
+        // Window attributes
         sf::Window *window;
         bool running;
+
+        // OpenGL attributes
+        GLuint vertexShader;
+        GLuint fragmentShader;
+        GLuint shaderProgram;
+        GLint posAttrib;
+
+        GLuint vao, vbo;
 
         // Vertex shader
         const GLchar* vertexSource =
         "#version 150 core\n"
         "in vec2 position;"
-        "in vec3 color;"
-        "out vec3 Color;"
-        "void main() {"
-            " Color = color;"
-            " gl_Position = vec4(position, 0.0, 1.0);"
+        "void main()"
+        "{"
+            "gl_Position = vec4(position, 0.0, 1.0);"
         "}";
 
         // Fragment shader
         const GLchar* fragmentSource =
         "#version 150 core\n"
-        "in vec3 Color;"
         "out vec4 outColor;"
-        "void main() {"
-            " outColor = vec4(Color, 1.0);"
+        "void main()"
+        "{"
+            "outColor = vec4(1.0, 1.0, 1.0, 1.0);"
         "}";
 
     public:
         // Initializes the game window
         Graphics();
 
+        // Frees data used by the object
+        ~Graphics();
+
         // Important function, needs beter description
         void run();
 
         // Checks user input and reacts accordingly
         void checkInput(sf::Event windowEvent);
+
+        /*
+         * Compiles the vertex shader. Leaves program
+         * if an error is detected during the process.
+         */
+        void compileVertexShader();
+
+        /*
+         * Compiles the fragment shader. Leaves program
+         * if an error is detected during the process.
+         */
+        void compileFragmentShader();
+
+        // Checks if ashader was correctly compiled
+        GLuint checkShader(GLuint shader);
+
+        // Combine vertex and fragment shaders into a program
+        void combineShaders();
+
+        // Makes some kind of link
+        void linkAttributes();
 };
--- src/graphics.cpp
+++ src/graphics.cpp
@@ -1,5 +1,12 @@
+#include <iostream>     // cout
 #include "graphics.hpp"
 #include "field.hpp"
+
+GLfloat vertices[] = {
+     0.0f,  0.5f,
+     0.5f, -0.5f,
+    -0.5f, -0.5f
+};
 
 /*-----------------------------------------------------------*/
 
@@ -13,6 +20,34 @@ Graphics::Graphics()
                             "Pong",
                             sf::Style::Close,
                             settings);
+
+    glewExperimental = GL_TRUE;
+    glewInit();
+
+    // Create Vertex Array Object
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+
+    // Create a Vertex Buffer Object and copy the vertex data to it
+    glGenBuffers(1, &vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    compileVertexShader();
+    compileFragmentShader();
+    combineShaders();
+    linkAttributes();
+}
+
+/*-----------------------------------------------------------*/
+
+Graphics::~Graphics()
+{
+    glDeleteProgram(shaderProgram);
+    glDeleteShader(fragmentShader);
+    glDeleteShader(vertexShader);
+    glDeleteBuffers(1, &vbo);
+    glDeleteVertexArrays(1, &vao);
 }
 
 /*-----------------------------------------------------------*/
@@ -23,11 +58,18 @@ void Graphics::run()
     running = true;
 
     while (running) {
+        // Clear the screen to black
+        glClearColor(0.0, 0.0, 0.0, 1.0);
+        glClear(GL_COLOR_BUFFER_BIT);
+
         sf::Event windowEvent;
         while (window->pollEvent(windowEvent))
             checkInput(windowEvent);
 
-        // TODO: Update game!
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
+        // Swap buffers
+        window->display();
     }
 }
 
@@ -49,4 +91,73 @@ void Graphics::checkInput(sf::Event windowEvent)
         default:
             break;
     }
+}
+
+/*-----------------------------------------------------------*/
+
+void Graphics::compileVertexShader()
+{
+    // Compile vertex shader
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexSource, NULL);
+    glCompileShader(vertexShader);
+
+    // Check if shader compiled correctly
+    if (checkShader(vertexShader) != GL_TRUE) {
+        std::cout << "Error when compiling vertex shader!\n";
+        exit(1);
+    }
+}
+
+/*-----------------------------------------------------------*/
+
+void Graphics::compileFragmentShader()
+{
+    // Compile fragment shader
+    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentSource, NULL);
+    glCompileShader(fragmentShader);
+
+    // Check if shader compiled correctly
+    if (checkShader(fragmentShader) != GL_TRUE) {
+        std::cout << "Error when compiling fragment shader!\n";
+        exit(1);
+    }
+}
+
+/*-----------------------------------------------------------*/
+
+GLuint Graphics::checkShader(GLuint shader)
+{
+    GLint status;
+    GLchar buffer[512];
+
+    glGetShaderInfoLog(shader, 512, NULL, buffer);
+    printf("%s\n", buffer);
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &status);
+
+    return status;
+}
+
+/*-----------------------------------------------------------*/
+
+void Graphics::combineShaders()
+{
+    shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+
+    glBindFragDataLocation(shaderProgram, 0, "outColor");
+
+    glLinkProgram(shaderProgram);
+    glUseProgram(shaderProgram);
+}
+
+/*-----------------------------------------------------------*/
+
+void Graphics::linkAttributes()
+{
+    posAttrib = glGetAttribLocation(shaderProgram, "position");
+    glVertexAttribPointer(posAttrib, 2, GL_FLOAT, GL_FALSE, 0, 0);
+    glEnableVertexAttribArray(posAttrib);
 }
